b53b64a70928178e7a5175ee498946396a1bc47d
BulldogX <qiu90123@163.com>
2020-06-02 12:59:31 +0800

    postcss-pxtorem minPixelValue: 1

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,15 @@
 const px2rem = require('postcss-pxtorem')
 
+/*
+ 如果某些布局，不想被转换，单位写成大写： PX
+ */
 module.exports = {
     plugins: [
         px2rem({
             rootValue: 75,
             unitPrecision: 5,
             propList:['*'],
+            minPixelValue: 1,
             selectorBlackList:['html','body']
         })
     ]
