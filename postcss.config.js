28dbe970c47eba078f49958cc890149d728a4fe1
BulldogX <qiu90123@163.com>
2020-06-02 12:53:40 +0800

    1、布局适配采用： vw + rem 2、retina   1px

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const px2rem = require('postcss-pxtorem')
+
+module.exports = {
+    plugins: [
+        px2rem({
+            rootValue: 75,
+            unitPrecision: 5,
+            propList:['*'],
+            selectorBlackList:['html','body']
+        })
+    ]
+}
