e56d9df53a863ce4aca90977e790e45cc84bcb86
BulldogX <qiu90123@163.com>
2020-06-16 12:55:33 +0800

    chore： 1、添加REACT_APP_ENV 环境变量 2、next-build-id

---
+++ config/index.js
@@ -0,0 +1,30 @@
+
+import getConfig from 'next/config'
+
+const development = {
+    baseURL: 'http://127.0.0.1:8776/'
+}
+const staging = {
+    baseURL: 'http://api.kede.com/staging'
+}
+const production = {
+    baseURL: 'http://api.kede.com/production'
+}
+
+const config = () => {
+    const {publicRuntimeConfig:{REACT_APP_ENV = 'production'}} = getConfig()
+
+    if (REACT_APP_ENV === 'development') {
+        // do development config
+        return development
+    } else if (REACT_APP_ENV === 'staging') {
+        // do staging config
+        return staging
+    } else {
+        // do production config
+        // e.g： analyse(false)
+        return production
+    }
+}
+
+export default config()
--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withLess = require('@zeit/next-less')
+const nextBuildId = require('next-build-id')
 
 module.exports = withLess({
   cssModules: true,
@@ -6,7 +7,18 @@ module.exports = withLess({
     importLoaders: 1,
     localIdentName: '[local]__[hash:base64:5]'
   },
-  webpack: function (config) {
+  webpack:(config) => {
     return config
+  },
+  /*
+    build id默认会在每次build时随机生成，如果在多台机器编译部署会导致不一致，前端缓存失效
+    保持同一个版本build id一致
+    */
+  generateBuildId: () => nextBuildId({dir: __dirname}),
+  /*nextjs gzip 交给nginx来处理，减轻server的压力*/
+  compress: false,
+  /* 增加自定义env环境变量*/
+  publicRuntimeConfig: {
+    REACT_APP_ENV: process.env.REACT_APP_ENV
   }
 })
